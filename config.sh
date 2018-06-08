1a49377cba5efa6e9a2af4e15c8b9a0b50b45ff9
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-06-08 19:06:35 -0400

    Try to fix cmake install

--- config.sh
+++ config.sh
@@ -9,11 +9,12 @@ function pre_build {
     if [ -n "$IS_OSX" ]; then
 	    brew update
 	    brew upgrade cmake || brew install cmake
-    # else
-    #         CMAKE_URL="http://www.cmake.org/files/v3.7/cmake-3.7.1-Linux-x86_64.tar.gz"
-    #         mkdir cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
-    #         export PATH=${TRAVIS_BUILD_DIR}/cmake/bin:${PATH}
+    else
+	    export CMAKE_URL="http://www.cmake.org/files/v3.7/cmake-3.7.1-Linux-x86_64.tar.gz"
+	    mkdir cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+	    export PATH=${TRAVIS_BUILD_DIR}/cmake/bin:${PATH}
     fi
+    # export PY_BIN = python -c "import sys; print(sys.executable[:-6])"
     cmake --version
 }
 
