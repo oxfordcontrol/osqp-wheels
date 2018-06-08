160e4bb314cdf202029e1de57e901400b7315659
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-06-08 18:01:29 -0400

    First commit with repos and multibuild

---
+++ README.md
@@ -0,0 +1,6 @@
+# OSQP
+
+This repository automates [OSQP](https://github.com/oxfordcontrol/osqp) wheel building using [multibuild](https://github.com/matthew-brett/multibuild), [Travis CI](https://travis-ci.org/oxfordcontrol/osqp-wheels), and [AppVeyor](https://ci.appveyor.com/project/bstellato/osqp-wheels).
+
+<!-- [![Build Status](https://travis-ci.org/cvxopt/cvxopt-wheels.svg?branch=master)](https://travis-ci.org/cvxopt/cvxopt-wheels) -->
+<!-- [![AppVeyor Build Status](https://ci.appveyor.com/api/projects/status/github/martinandersen/cvxopt-wheels?branch=master&svg=true)](https://ci.appveyor.com/project/martinandersen/cvxopt-wheels) -->
---
+++ config.sh
@@ -0,0 +1,27 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+
+    # Install CMake
+    if [ -n "$IS_OSX" ]; then
+	    brew update
+	    brew upgrade cmake || brew install cmake
+    else
+	    CMAKE_URL="http://www.cmake.org/files/v3.7/cmake-3.7.1-Linux-x86_64.tar.gz"
+	    mkdir cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C cmake
+	    export PATH=${TRAVIS_BUILD_DIR}/cmake/bin:${PATH}
+    fi
+    cmake --version
+}
+
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    # python -c 'import sys; import yourpackage; sys.exit(yourpackage.test())'
+    pytest
+}
+
