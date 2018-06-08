5b1e1acdd89fccca4fc81e81ca7fef77fdf12bec
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-06-08 18:22:34 -0400

    Try to fix pytst

--- config.sh
+++ config.sh
@@ -22,6 +22,6 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     python --version
     # python -c 'import sys; import yourpackage; sys.exit(yourpackage.test())'
-    pytest
+    cd ${TRAVIS_BUILD_DIR}/osqp-python; pytest
 }
 
