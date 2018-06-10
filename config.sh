553fb010060549de0dcf4dd51efc9c3896a86c47
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-06-10 16:18:03 -0400

    Try pip cmake install travis

--- config.sh
+++ config.sh
@@ -6,42 +6,56 @@ CMAKE_VER_PATCH="2"
 CMAKE_VER_FULL="${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}"
 TESTS_DIR="$(pwd)/osqp-python"
 
+
+function install_cmake {
+	# Install CMake
+	# if [ -n "$IS_OSX" ]; then
+	#         brew update
+	#         brew upgrade cmake || brew install cmake
+	# else
+	#         # Unpack cmake
+	#         # CMAKE_NAME="cmake-${CMAKE_VER_FULL}-Linux-x86_64"
+	#         # CMAKE_URL="http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/${CMAKE_NAME}.tar.gz"
+	#         # fetch_unpack ${CMAKE_URL}
+	#         # export PATH=`pwd`/${CMAKE_NAME}/bin:${PATH}
+        #
+	#         # Build cmake and zlib (required by cmake)
+	#         yum install -y zlib-dev
+	#         fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
+	#         (cd cmake-${CMAKE_VER_FULL} \
+	#                     && ./bootstrap --system-curl \
+	#                     && make \
+	#                     && make install)
+	# fi
+
+        # Get python binary location and add it to the path
+	PYBIN=python -c 'import sys; print(sys.executable[:-6])'
+	rm -rf /usr/local/bin/cmake
+	ln -s $PYBIN/cmake /usr/local/bin/cmake
+
+        # Check cmake version
+	cmake --version
+}
+
+
 function pre_build {
 	# Any stuff that you need to do before you start building the wheels
 	# Runs in the root directory of this repository.
+	install_cmake
 
 
-	# Install CMake
-	if [ -n "$IS_OSX" ]; then
-		brew update
-		brew upgrade cmake || brew install cmake
-	else
-		# Unpack cmake
-		# CMAKE_NAME="cmake-${CMAKE_VER_FULL}-Linux-x86_64"
-		# CMAKE_URL="http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/${CMAKE_NAME}.tar.gz"
-		# fetch_unpack ${CMAKE_URL}
-		# export PATH=`pwd`/${CMAKE_NAME}/bin:${PATH}
-
-		# Build cmake and zlib (required by cmake)
-		build_zlib
-		fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
-		(cd cmake-${CMAKE_VER_FULL} \
-			    && ./bootstrap --system-curl \
-			    && make \
-			    && make install)
-	fi
-	cmake --version
 }
 
 
 function run_tests {
 	# Build cmake and zlib (required by cmake)
-	build_zlib
-	fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
-	(cd cmake-${CMAKE_VER_FULL} \
-		&& ./bootstrap --system-curl \
-		&& make \
-		&& make install)
+	install_cmake
+	# yum install -y zlib-dev
+	# fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
+	# (cd cmake-${CMAKE_VER_FULL} \
+	#         && ./bootstrap --system-curl \
+	#         && make \
+	#         && make install)
 
 	# Add Cmake to docker
 	# CMAKE_NAME="cmake-${CMAKE_VER_FULL}-Linux-x86_64"
