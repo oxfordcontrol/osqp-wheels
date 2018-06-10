8cff44543b035372e079d1bd953261305886244b
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2018-06-10 16:32:58 -0400

    Try to fix ci AGAIN

--- config.sh
+++ config.sh
@@ -9,29 +9,29 @@ TESTS_DIR="$(pwd)/osqp-python"
 
 function install_cmake {
 	# Install CMake
-	# if [ -n "$IS_OSX" ]; then
-	#         brew update
-	#         brew upgrade cmake || brew install cmake
-	# else
-	#         # Unpack cmake
-	#         # CMAKE_NAME="cmake-${CMAKE_VER_FULL}-Linux-x86_64"
-	#         # CMAKE_URL="http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/${CMAKE_NAME}.tar.gz"
-	#         # fetch_unpack ${CMAKE_URL}
-	#         # export PATH=`pwd`/${CMAKE_NAME}/bin:${PATH}
-        #
-	#         # Build cmake and zlib (required by cmake)
-	#         yum install -y zlib-dev
-	#         fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
-	#         (cd cmake-${CMAKE_VER_FULL} \
-	#                     && ./bootstrap --system-curl \
-	#                     && make \
-	#                     && make install)
-	# fi
+	if [ -n "$IS_OSX" ]; then
+		brew update
+		brew upgrade cmake || brew install cmake
+	else
+		# Unpack cmake
+		# CMAKE_NAME="cmake-${CMAKE_VER_FULL}-Linux-x86_64"
+		# CMAKE_URL="http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/${CMAKE_NAME}.tar.gz"
+		# fetch_unpack ${CMAKE_URL}
+		# export PATH=`pwd`/${CMAKE_NAME}/bin:${PATH}
 
-        # Get python binary location and add it to the path
-	PYBIN=`python -c 'import sys; print(sys.executable[:-6])'`
-	rm -rf /usr/local/bin/cmake
-	ln -s $PYBIN/cmake /usr/local/bin/cmake
+		# Build cmake and zlib (required by cmake)
+		yum install -y zlib-dev
+		fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
+		(cd cmake-${CMAKE_VER_FULL} \
+			    && ./bootstrap --system-curl \
+			    && make \
+			    && make install)
+	fi
+
+        # # Get python binary location and add it to the path
+	# PYBIN=`python -c 'import sys; print(sys.executable[:-6])'`
+	# rm -rf /usr/local/bin/cmake
+	# ln -s $PYBIN/cmake /usr/local/bin/cmake
 
         # Check cmake version
 	cmake --version
@@ -41,15 +41,24 @@ function install_cmake {
 function pre_build {
 	# Any stuff that you need to do before you start building the wheels
 	# Runs in the root directory of this repository.
-	install_cmake
+	# install_cmake
 
 
 }
 
 
 function run_tests {
+
+        # Get python binary location and add it to the path
+	PYBIN=`python -c 'import sys; print(sys.executable[:-6])'`
+	rm -rf /usr/local/bin/cmake
+	ln -s $PYBIN/cmake /usr/local/bin/cmake
+
+        # Check cmake version
+	cmake --version
+
 	# Build cmake and zlib (required by cmake)
-	install_cmake
+	# install_cmake
 	# yum install -y zlib-dev
 	# fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
 	# (cd cmake-${CMAKE_VER_FULL} \
