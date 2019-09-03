46560bd6e46e3369430ec3fb05fab43304bf9fcc
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-09-03 09:02:28 -0400

    another try

--- config.sh
+++ config.sh
@@ -7,37 +7,32 @@ CMAKE_VER_FULL="${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}"
 TESTS_DIR="$(pwd)/osqp-python"
 
 
-function install_cmake {
-	# Install CMake
-	if [ -n "$IS_OSX" ]; then
-		brew update
-		brew upgrade cmake || brew install cmake
-	else
-		# Build cmake and zlib (required by cmake)
-		yum install -y zlib-dev
-		fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
-		(cd cmake-${CMAKE_VER_FULL} \
-			    && ./bootstrap --system-curl \
-			    && make \
-			    && make install)
-	fi
-
-
-        # Check cmake version
-	cmake --version
-}
+# function install_cmake {
+#         # Install CMake
+#         if [ -n "$IS_OSX" ]; then
+#                 brew update
+#                 brew upgrade cmake || brew install cmake
+#         else
+#                 # Build cmake and zlib (required by cmake)
+#                 yum install -y zlib-dev
+#                 fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
+#                 (cd cmake-${CMAKE_VER_FULL} \
+#                             && ./bootstrap --system-curl \
+#                             && make \
+#                             && make install)
+#         fi
+#
+#
+#         # Check cmake version
+#         cmake --version
+# }
 
 
 function pre_build {
 	# Any stuff that you need to do before you start building the wheels
 	# Runs in the root directory of this repository.
+
 	# install_cmake
-	:
-}
-
-
-function run_tests {
-
         # Get python binary location and add it to the path
 	PYBIN=`python -c 'import sys; print(sys.executable[:-6])'`
 	rm -rf /usr/local/bin/cmake
@@ -45,6 +40,10 @@ function run_tests {
 
         # Check cmake version
 	cmake --version
+}
+
+
+function run_tests {
 
 	# Runs tests on installed distribution from an empty directory
 	python --version
