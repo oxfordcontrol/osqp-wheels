24a6a862c9704f54c938bdc50708c1e47c9372ca
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-09-03 16:00:20 -0400

    another try

--- config.sh
+++ config.sh
@@ -13,21 +13,17 @@ function install_cmake {
 		brew update
 		brew upgrade cmake || brew install cmake
 	else
-		curl -LO https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2-Linux-x86_64.sh
-		chmod +x cmake-3.15.2-Linux-x86_64.sh
-		./cmake-3.15.2-Linux-x86_64.sh --prefix=/usr --skip-license --exclude-subdir
-
-		# # Build cmake and zlib (required by cmake)
-		# yum install -y zlib-devel
-		# fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
-		# (cd cmake-${CMAKE_VER_FULL} \
-		#             && ./bootstrap --system-curl \
-		#             && make \
-		#             && make install)
+		# Build cmake and zlib (required by cmake)
+		yum install -y zlib-dev
+		fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
+		(cd cmake-${CMAKE_VER_FULL} \
+			    && ./bootstrap --system-curl \
+			    && make \
+			    && make install)
 	fi
 
 
-	# Check cmake version
+        # Check cmake version
 	cmake --version
 }
 
@@ -35,25 +31,22 @@ function install_cmake {
 function pre_build {
 	# Any stuff that you need to do before you start building the wheels
 	# Runs in the root directory of this repository.
-	install_cmake
-
-	# Does not work
-        # Get python binary location and add it to the path
-	# pip install cmake
-	# echo `which cmake`
-	# PYBIN=`python -c 'import sys; print(sys.executable[:-6])'`
-	# ln -sf $PYBIN/cmake /usr/local/bin/cmake
-	# echo `which cmake`
-        #
-	# # Check cmake version
-	cmake --version
+	# install_cmake
+	:
 }
 
 
 function run_tests {
+
+        # Get python binary location and add it to the path
+	PYBIN=`python -c 'import sys; print(sys.executable[:-6])'`
+	ln -sf $PYBIN/cmake /usr/local/bin/cmake
+
+        # Check cmake version
+	cmake --version
+
 	# Runs tests on installed distribution from an empty directory
 	python --version
-	# Disable MKL since not in docker image
+	# python -c 'import sys; import yourpackage; sys.exit(yourpackage.test())'
 	cd ${TESTS_DIR}; python -m pytest -k 'not mkl_'
 }
-
