9515488792033d6cc13b2c904cbb7c40032b73fc
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-09-03 10:16:00 -0400

    Install cmake sources

--- config.sh
+++ config.sh
@@ -7,40 +7,41 @@ CMAKE_VER_FULL="${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}"
 TESTS_DIR="$(pwd)/osqp-python"
 
 
-# function install_cmake {
-#         # Install CMake
-#         if [ -n "$IS_OSX" ]; then
-#                 brew update
-#                 brew upgrade cmake || brew install cmake
-#         else
-#                 # Build cmake and zlib (required by cmake)
-#                 yum install -y zlib-dev
-#                 fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
-#                 (cd cmake-${CMAKE_VER_FULL} \
-#                             && ./bootstrap --system-curl \
-#                             && make \
-#                             && make install)
-#         fi
-#
-#
-#         # Check cmake version
-#         cmake --version
-# }
+function install_cmake {
+	# Install CMake
+	if [ -n "$IS_OSX" ]; then
+		brew update
+		brew upgrade cmake || brew install cmake
+	else
+		# Build cmake and zlib (required by cmake)
+		yum install -y zlib-dev
+		fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
+		(cd cmake-${CMAKE_VER_FULL} \
+			    && ./bootstrap --system-curl \
+			    && make \
+			    && make install)
+	fi
+
+
+	# Check cmake version
+	cmake --version
+}
 
 
 function pre_build {
 	# Any stuff that you need to do before you start building the wheels
 	# Runs in the root directory of this repository.
+	install_cmake
 
-	# install_cmake
+	# Does not work
         # Get python binary location and add it to the path
-	pip install cmake
-	PYBIN=`python -c 'import sys; print(sys.executable[:-6])'`
-	rm -rf /usr/local/bin/cmake
-	ln -s $PYBIN/cmake /usr/local/bin/cmake
+	# pip install cmake
+	# PYBIN=`python -c 'import sys; print(sys.executable[:-6])'`
+	# rm -rf /usr/local/bin/cmake
+	# ln -s $PYBIN/cmake /usr/local/bin/cmake
 
-        # Check cmake version
-	cmake --version
+	# Check cmake version
+	# cmake --version
 }
 
 
