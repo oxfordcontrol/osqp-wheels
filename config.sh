7b4f3ade4c1e69419c9ff49f8de2477d9dc3dafa
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-09-05 12:55:00 -0400

    simplify

--- config.sh
+++ config.sh
@@ -1,49 +1,51 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
-CMAKE_VER_MAJ="3"
-CMAKE_VER_MIN="9"
-CMAKE_VER_PATCH="2"
-CMAKE_VER_FULL="${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}"
+# CMAKE_VER_MAJ="3"
+# CMAKE_VER_MIN="9"
+# CMAKE_VER_PATCH="2"
+# CMAKE_VER_FULL="${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}"
 TESTS_DIR="$(pwd)/osqp-python"
 
 
-function install_cmake {
-	# Install CMake
-	if [ -n "$IS_OSX" ]; then
-		brew update
-		brew upgrade cmake || brew install cmake
-	else
-		# Build cmake and zlib (required by cmake)
-		yum install -y zlib-dev
-		fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
-		(cd cmake-${CMAKE_VER_FULL} \
-			    && ./bootstrap --system-curl \
-			    && make \
-			    && make install)
-	fi
-
-
-        # Check cmake version
-	cmake --version
-}
+# function install_cmake {
+#         # Install CMake
+#         if [ -n "$IS_OSX" ]; then
+#                 brew update
+#                 brew upgrade cmake || brew install cmake
+#         else
+#                 # Build cmake and zlib (required by cmake)
+#                 yum install -y zlib-dev
+#                 fetch_unpack http://www.cmake.org/files/v${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}/cmake-${CMAKE_VER_MAJ}.${CMAKE_VER_MIN}.${CMAKE_VER_PATCH}.tar.gz
+#                 (cd cmake-${CMAKE_VER_FULL} \
+#                             && ./bootstrap --system-curl \
+#                             && make \
+#                             && make install)
+#         fi
+#
+#
+#         # Check cmake version
+#         cmake --version
+# }
 
 
 function pre_build {
 	# Any stuff that you need to do before you start building the wheels
 	# Runs in the root directory of this repository.
 	# install_cmake
-	:
+	if [ -z "$IS_OSX" ]; then
+	    pip install cmake  # Version in manylinux1 container too old.
+	fi
 }
 
 
 function run_tests {
 
         # Get python binary location and add it to the path
-	PYBIN=`python -c 'import sys; print(sys.executable[:-6])'`
-	ln -sf $PYBIN/cmake /usr/local/bin/cmake
+	# PYBIN=`python -c 'import sys; print(sys.executable[:-6])'`
+	# ln -sf $PYBIN/cmake /usr/local/bin/cmake
 
         # Check cmake version
-	cmake --version
+	# cmake --version
 
 	# Runs tests on installed distribution from an empty directory
 	python --version
