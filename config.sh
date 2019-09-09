3e62d1b1b1031dad4c5bd82143063762a8ff5631
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-09-09 14:14:00 -0400

    another try

--- config.sh
+++ config.sh
@@ -4,16 +4,16 @@
 function pre_build {
 	# Any stuff that you need to do before you start building the wheels
 	# Runs in the root directory of this repository.
-	:
+
+	# Fix cmake installation linking the appropriate binary
+	pip install cmake
+	rm `python -c 'import sys; print(sys.executable[:-6])'`cmake
+	CMAKE_BIN=`python -c "import site; print(site.getsitepackages()[0])"`/cmake/data/bin/cmake
+	ln -sf ${CMAKE_BIN} /usr/bin/cmake
 }
 
 
 function run_tests {
-	# Fix cmake installation linking the appropriate binary
-	rm `python -c 'import sys; print(sys.executable[:-6])'`cmake
-	CMAKE_BIN=`python -c "import site; print(site.getsitepackages()[0])"`/cmake/data/bin/cmake
-	ln -sf ${CMAKE_BIN} /usr/bin/cmake
-
 	# Runs tests on installed distribution from an empty directory
 	python --version
 	# python -c 'import sys; import yourpackage; sys.exit(yourpackage.test())'
